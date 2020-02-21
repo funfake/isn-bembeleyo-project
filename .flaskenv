5436f95bf736dd745bfd4ace0a3e40ac9b6096ec
William BRISA <funfake.electronics@gmail.com>
2020-02-21 12:00:50 +0100

    base.html index.html pages were created, began to work on route + wtforms

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,3 @@
 FLASK_APP=bembeleyo.py
+FLASK_DEBUG=1
+FLASK_ENV=development
--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,14 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'William'}
-    return render_template('index.html', title='Accueil', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Accueil', user=user, posts=posts)
